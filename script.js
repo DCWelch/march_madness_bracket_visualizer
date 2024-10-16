a28b22a33e41b28602f1692a044fe2b3fbfbacda
DCWelch <dcwelch545@gmail.com>
2024-10-16 23:57:55 +0100

    Small updates

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const teamData = {
   "teams": [
+    /* East */
     {
       "id": "team_01",
       "region": "East",
@@ -28,6 +29,91 @@ const teamData = {
       "team_name": "Northwestern",
       "logo_url": "logos/northwestern.svg"
     },
+	{
+      "id": "team_05",
+      "region": "East",
+      "seed": 5,
+      "team_name": "San Diego St.",
+      "logo_url": "logos/san-diego-st.svg"
+    },
+    {
+      "id": "team_06",
+      "region": "East",
+      "seed": 12,
+      "team_name": "UAB",
+      "logo_url": "logos/uab.svg"
+    },
+    {
+      "id": "team_07",
+      "region": "East",
+      "seed": 4,
+      "team_name": "Auburn",
+      "logo_url": "logos/auburn.svg"
+    },
+    {
+      "id": "team_08",
+      "region": "East",
+      "seed": 13,
+      "team_name": "Yale",
+      "logo_url": "logos/yale.svg"
+    },
+	{
+      "id": "team_09",
+      "region": "East",
+      "seed": 6,
+      "team_name": "BYU",
+      "logo_url": "logos/byu.svg"
+    },
+    {
+      "id": "team_10",
+      "region": "East",
+      "seed": 11,
+      "team_name": "Duquesne",
+      "logo_url": "logos/duquesne.svg"
+    },
+    {
+      "id": "team_11",
+      "region": "East",
+      "seed": 3,
+      "team_name": "Illinois",
+      "logo_url": "logos/illinois.svg"
+    },
+    {
+      "id": "team_12",
+      "region": "East",
+      "seed": 14,
+      "team_name": "Morehead St.",
+      "logo_url": "logos/morehead-st.svg"
+    },
+	{
+      "id": "team_13",
+      "region": "East",
+      "seed": 7,
+      "team_name": "Washington St.",
+      "logo_url": "logos/washington-st.svg"
+    },
+    {
+      "id": "team_14",
+      "region": "East",
+      "seed": 10,
+      "team_name": "Drake",
+      "logo_url": "logos/drake.svg"
+    },
+    {
+      "id": "team_15",
+      "region": "East",
+      "seed": 2,
+      "team_name": "Iowa St.",
+      "logo_url": "logos/iowa-st.svg"
+    },
+    {
+      "id": "team_16",
+      "region": "East",
+      "seed": 15,
+      "team_name": "S. Dakota St.",
+      "logo_url": "logos/south-dakota-st.svg"
+    },
+	/* West */
     {
       "id": "team_17",
       "region": "West",
@@ -55,6 +141,316 @@ const teamData = {
       "seed": 9,
       "team_name": "Michigan St.",
       "logo_url": "logos/michigan-st.svg"
+    },
+	{
+      "id": "team_21",
+      "region": "West",
+      "seed": 5,
+      "team_name": "St. Mary's",
+      "logo_url": "logos/st-marys-ca.svg"
+    },
+    {
+      "id": "team_22",
+      "region": "West",
+      "seed": 12,
+      "team_name": "Grand Canyon",
+      "logo_url": "logos/grand-canyon.svg"
+    },
+    {
+      "id": "team_23",
+      "region": "West",
+      "seed": 4,
+      "team_name": "Alabama",
+      "logo_url": "logos/alabama.svg"
+    },
+    {
+      "id": "team_24",
+      "region": "West",
+      "seed": 13,
+      "team_name": "Charleston",
+      "logo_url": "logos/col-of-charleston.svg"
+    },
+	{
+      "id": "team_25",
+      "region": "West",
+      "seed": 6,
+      "team_name": "Clemson",
+      "logo_url": "logos/clemson.svg"
+    },
+    {
+      "id": "team_26",
+      "region": "West",
+      "seed": 11,
+      "team_name": "New Mexico",
+      "logo_url": "logos/new-mexico.svg"
+    },
+    {
+      "id": "team_27",
+      "region": "West",
+      "seed": 3,
+      "team_name": "Baylor",
+      "logo_url": "logos/baylor.svg"
+    },
+    {
+      "id": "team_28",
+      "region": "West",
+      "seed": 14,
+      "team_name": "Colgate",
+      "logo_url": "logos/colgate.svg"
+    },
+	{
+      "id": "team_29",
+      "region": "West",
+      "seed": 7,
+      "team_name": "Dayton",
+      "logo_url": "logos/dayton.svg"
+    },
+    {
+      "id": "team_30",
+      "region": "West",
+      "seed": 10,
+      "team_name": "Nevada",
+      "logo_url": "logos/nevada.svg"
+    },
+    {
+      "id": "team_31",
+      "region": "West",
+      "seed": 2,
+      "team_name": "Arizona",
+      "logo_url": "logos/arizona.svg"
+    },
+    {
+      "id": "team_32",
+      "region": "West",
+      "seed": 15,
+      "team_name": "Long Beach St.",
+      "logo_url": "logos/long-beach-st.svg"
+    },
+	/* South */
+	{
+      "id": "team_33",
+      "region": "South",
+      "seed": 1,
+      "team_name": "Houston",
+      "logo_url": "logos/houston.svg"
+    },
+    {
+      "id": "team_34",
+      "region": "South",
+      "seed": 16,
+      "team_name": "Longwood",
+      "logo_url": "logos/longwood.svg"
+    },
+    {
+      "id": "team_35",
+      "region": "South",
+      "seed": 8,
+      "team_name": "Nebraska",
+      "logo_url": "logos/nebraska.svg"
+    },
+    {
+      "id": "team_36",
+      "region": "South",
+      "seed": 9,
+      "team_name": "Texas A&M",
+      "logo_url": "logos/texas-am.svg"
+    },
+	{
+      "id": "team_37",
+      "region": "South",
+      "seed": 5,
+      "team_name": "Wisconsin",
+      "logo_url": "logos/wisconsin.svg"
+    },
+    {
+      "id": "team_38",
+      "region": "South",
+      "seed": 12,
+      "team_name": "James Madison",
+      "logo_url": "logos/james-madison.svg"
+    },
+    {
+      "id": "team_39",
+      "region": "South",
+      "seed": 4,
+      "team_name": "Duke",
+      "logo_url": "logos/duke.svg"
+    },
+    {
+      "id": "team_40",
+      "region": "South",
+      "seed": 13,
+      "team_name": "Vermont",
+      "logo_url": "logos/vermont.svg"
+    },
+	{
+      "id": "team_41",
+      "region": "South",
+      "seed": 6,
+      "team_name": "Texas Tech",
+      "logo_url": "logos/texas-tech.svg"
+    },
+    {
+      "id": "team_42",
+      "region": "South",
+      "seed": 11,
+      "team_name": "NC State",
+      "logo_url": "logos/north-carolina-st.svg"
+    },
+    {
+      "id": "team_43",
+      "region": "South",
+      "seed": 3,
+      "team_name": "Kentucky",
+      "logo_url": "logos/kentucky.svg"
+    },
+    {
+      "id": "team_44",
+      "region": "South",
+      "seed": 14,
+      "team_name": "Oakland",
+      "logo_url": "logos/oakland.svg"
+    },
+	{
+      "id": "team_45",
+      "region": "South",
+      "seed": 7,
+      "team_name": "Florida",
+      "logo_url": "logos/florida.svg"
+    },
+    {
+      "id": "team_46",
+      "region": "South",
+      "seed": 10,
+      "team_name": "Colorado",
+      "logo_url": "logos/colorado.svg"
+    },
+    {
+      "id": "team_47",
+      "region": "South",
+      "seed": 2,
+      "team_name": "Marquette",
+      "logo_url": "logos/marquette.svg"
+    },
+    {
+      "id": "team_48",
+      "region": "South",
+      "seed": 15,
+      "team_name": "Western Ky.",
+      "logo_url": "logos/western-ky.svg"
+    },
+	/* Midwest */
+	{
+      "id": "team_49",
+      "region": "Midwest",
+      "seed": 1,
+      "team_name": "Purdue",
+      "logo_url": "logos/purdue.svg"
+    },
+    {
+      "id": "team_50",
+      "region": "Midwest",
+      "seed": 16,
+      "team_name": "Grambling St.",
+      "logo_url": "logos/grambling.svg"
+    },
+    {
+      "id": "team_51",
+      "region": "Midwest",
+      "seed": 8,
+      "team_name": "Utah St.",
+      "logo_url": "logos/utah-st.svg"
+    },
+    {
+      "id": "team_52",
+      "region": "Midwest",
+      "seed": 9,
+      "team_name": "TCU",
+      "logo_url": "logos/tcu.svg"
+    },
+	{
+      "id": "team_53",
+      "region": "Midwest",
+      "seed": 5,
+      "team_name": "Gonzaga",
+      "logo_url": "logos/gonzaga.svg"
+    },
+    {
+      "id": "team_54",
+      "region": "Midwest",
+      "seed": 12,
+      "team_name": "McNeese",
+      "logo_url": "logos/mcneese.svg"
+    },
+    {
+      "id": "team_55",
+      "region": "Midwest",
+      "seed": 4,
+      "team_name": "Kansas",
+      "logo_url": "logos/kansas.svg"
+    },
+    {
+      "id": "team_56",
+      "region": "Midwest",
+      "seed": 13,
+      "team_name": "Samford",
+      "logo_url": "logos/samford.svg"
+    },
+	{
+      "id": "team_57",
+      "region": "Midwest",
+      "seed": 6,
+      "team_name": "S. Carolina",
+      "logo_url": "logos/south-carolina.svg"
+    },
+    {
+      "id": "team_58",
+      "region": "Midwest",
+      "seed": 11,
+      "team_name": "Oregon",
+      "logo_url": "logos/oregon.svg"
+    },
+    {
+      "id": "team_59",
+      "region": "Midwest",
+      "seed": 3,
+      "team_name": "Creighton",
+      "logo_url": "logos/creighton.svg"
+    },
+    {
+      "id": "team_60",
+      "region": "Midwest",
+      "seed": 14,
+      "team_name": "Akron",
+      "logo_url": "logos/akron.svg"
+    },
+	{
+      "id": "team_61",
+      "region": "Midwest",
+      "seed": 7,
+      "team_name": "Texas",
+      "logo_url": "logos/texas.svg"
+    },
+    {
+      "id": "team_62",
+      "region": "Midwest",
+      "seed": 10,
+      "team_name": "Colorado St.",
+      "logo_url": "logos/colorado-st.svg"
+    },
+    {
+      "id": "team_63",
+      "region": "Midwest",
+      "seed": 2,
+      "team_name": "Tennessee",
+      "logo_url": "logos/tennessee.svg"
+    },
+    {
+      "id": "team_64",
+      "region": "Midwest",
+      "seed": 15,
+      "team_name": "Saint Peter's",
+      "logo_url": "logos/st-peters.svg"
     }
   ]
 };
@@ -74,8 +470,8 @@ function drawBracketLine(game1, game2, variant) {
       startY = ((rect1.top + rect1.height / 2 - containerRect.top) / containerRect.height) * 100;
       endX = ((rect2.left + rect2.width / 2 - containerRect.left) / containerRect.width) * 100;
       endY = ((rect2.top - containerRect.top) / containerRect.height) * 100;
-	  drawSVGLine(`${startX}%`, `${startY}%`, `${endX}%`, `${startY}%`, '#ccc', 'round', 'square', '1');
-      drawSVGLine(`${endX}%`, `${startY}%`, `${endX}%`, `${endY}%`, '#ccc', 'round', 'square', '1');
+	  drawSVGLine(`${startX}%`, `${startY}%`, `${endX}%`, `${startY}%`, '#ccc', 'square', 'square', '1');
+      drawSVGLine(`${endX}%`, `${startY}%`, `${endX}%`, `${endY}%`, '#ccc', 'square', 'square', '1');
       break;
       
     case 'left-down':
@@ -83,8 +479,8 @@ function drawBracketLine(game1, game2, variant) {
       startY = ((rect1.top + rect1.height / 2 - containerRect.top) / containerRect.height) * 100;
       endX = ((rect2.left + rect2.width / 2 - containerRect.left) / containerRect.width) * 100;
       endY = ((rect2.top - containerRect.top) / containerRect.height) * 100;
-	  drawSVGLine(`${startX}%`, `${startY}%`, `${endX}%`, `${startY}%`, '#ccc', 'round', 'square', '1');
-      drawSVGLine(`${endX}%`, `${startY}%`, `${endX}%`, `${endY}%`, '#ccc', 'round', 'square', '1');
+	  drawSVGLine(`${startX}%`, `${startY}%`, `${endX}%`, `${startY}%`, '#ccc', 'square', 'square', '1');
+      drawSVGLine(`${endX}%`, `${startY}%`, `${endX}%`, `${endY}%`, '#ccc', 'square', 'square', '1');
       break;
 
     case 'right-up':
@@ -92,8 +488,8 @@ function drawBracketLine(game1, game2, variant) {
       startY = ((rect1.top + rect1.height / 2 - containerRect.top) / containerRect.height) * 100;
       endX = ((rect2.left + rect2.width / 2 - containerRect.left) / containerRect.width) * 100;
       endY = ((rect2.bottom - containerRect.top) / containerRect.height) * 100;
-	  drawSVGLine(`${startX}%`, `${startY}%`, `${endX}%`, `${startY}%`, '#ccc', 'round', 'square', '1');
-      drawSVGLine(`${endX}%`, `${startY}%`, `${endX}%`, `${endY}%`, '#ccc', 'round', 'square', '1');
+	  drawSVGLine(`${startX}%`, `${startY}%`, `${endX}%`, `${startY}%`, '#ccc', 'square', 'square', '1');
+      drawSVGLine(`${endX}%`, `${startY}%`, `${endX}%`, `${endY}%`, '#ccc', 'square', 'square', '1');
       break;
 
     case 'left-up':
@@ -101,8 +497,8 @@ function drawBracketLine(game1, game2, variant) {
       startY = ((rect1.top + rect1.height / 2 - containerRect.top) / containerRect.height) * 100;
       endX = ((rect2.left + rect2.width / 2 - containerRect.left) / containerRect.width) * 100;
       endY = ((rect2.bottom - containerRect.top) / containerRect.height) * 100;
-	  drawSVGLine(`${startX}%`, `${startY}%`, `${endX}%`, `${startY}%`, '#ccc', 'round', 'square', '1');
-      drawSVGLine(`${endX}%`, `${startY}%`, `${endX}%`, `${endY}%`, '#ccc', 'round', 'square', '1');
+	  drawSVGLine(`${startX}%`, `${startY}%`, `${endX}%`, `${startY}%`, '#ccc', 'square', 'square', '1');
+      drawSVGLine(`${endX}%`, `${startY}%`, `${endX}%`, `${endY}%`, '#ccc', 'square', 'square', '1');
       break;
 
     case 'right':
@@ -111,9 +507,9 @@ function drawBracketLine(game1, game2, variant) {
       midX = ((rect1.right + rect2.left) / 2 - containerRect.left) / containerRect.width * 100;
       midY = ((rect2.top + rect2.height / 4 - containerRect.top) / containerRect.height) * 100;
       endX = ((rect2.left - containerRect.left) / containerRect.width) * 100;
-      drawSVGLine(`${startX}%`, `${startY}%`, `${midX}%`, `${startY}%`, '#ccc', 'round', 'square', '1');
-      drawSVGLine(`${midX}%`, `${startY}%`, `${midX}%`, `${midY}%`, '#ccc', 'round', 'square', '1');
-      drawSVGLine(`${midX}%`, `${midY}%`, `${endX}%`, `${midY}%`, '#ccc', 'round', 'square', '1');
+      drawSVGLine(`${startX}%`, `${startY}%`, `${midX}%`, `${startY}%`, '#ccc', 'square', 'square', '1');
+      drawSVGLine(`${midX}%`, `${startY}%`, `${midX}%`, `${midY}%`, '#ccc', 'square', 'square', '1');
+      drawSVGLine(`${midX}%`, `${midY}%`, `${endX}%`, `${midY}%`, '#ccc', 'square', 'square', '1');
       break;
 
     case 'left':
@@ -122,9 +518,9 @@ function drawBracketLine(game1, game2, variant) {
       midX = ((rect1.left + rect2.right) / 2 - containerRect.left) / containerRect.width * 100;
       midY = ((rect2.top + (3 * rect2.height) / 4 - containerRect.top) / containerRect.height) * 100;
       endX = ((rect2.right - containerRect.left) / containerRect.width) * 100;
-      drawSVGLine(`${startX}%`, `${startY}%`, `${midX}%`, `${startY}%`, '#ccc', 'round', 'square', '1');
-      drawSVGLine(`${midX}%`, `${startY}%`, `${midX}%`, `${midY}%`, '#ccc', 'round', 'square', '1');
-      drawSVGLine(`${midX}%`, `${midY}%`, `${endX}%`, `${midY}%`, '#ccc', 'round', 'square', '1');
+      drawSVGLine(`${startX}%`, `${startY}%`, `${midX}%`, `${startY}%`, '#ccc', 'square', 'square', '1');
+      drawSVGLine(`${midX}%`, `${startY}%`, `${midX}%`, `${midY}%`, '#ccc', 'square', 'square', '1');
+      drawSVGLine(`${midX}%`, `${midY}%`, `${endX}%`, `${midY}%`, '#ccc', 'square', 'square', '1');
       break;
     
     default:
